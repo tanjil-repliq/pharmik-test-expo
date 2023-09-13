81111166cb0542ebc8620001f05792dbe34b3c37
muntashir-repliq <muntasir.wahid@repliq.dev>
2023-09-13 17:19:58 +0600

    create top nav

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,24 @@
 import { Link } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+
+import { Text, View, ScrollView } from "react-native";
 
 export default function LandingPage() {
   return (
-    <View className="flex justify-center items-center min-h-screen">
-      <Text className="text-red-400 text-center text-4xl">
-        Pharmik comming soon...
-      </Text>
-      <Link href="/login">Go to Login</Link>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView className="min-h-screen">
+      <View className="w-ful p-4 flex-row items-center justify-between">
+        <Text className="text-primary font-semibold text-lg">Pharmik</Text>
+        <View className="flex-row items-center gap-4">
+          <Link href="/login" className="text-primary font-semibold">
+            Login
+          </Link>
+          <Link
+            href="/login"
+            className="rounded-md bg-primary py-2 px-3 font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+          >
+            Sighup
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f766e",
+        "primary-bg": "#F7FDFC",
+        "primary-200": "#90D5D2",
+        "primary-100": "#E0F5F4",
+        warning: "#E30909",
+        "grey-700": "#333333",
+        "grey-500": "#666666",
+        "grey-400": "#999999",
+        "grey-300": "#CCCCCC",
+        "grey-200": "#E1E1E1",
+        "grey-50": "#FAFAFA",
+      },
+    },
   },
   plugins: [],
 };
