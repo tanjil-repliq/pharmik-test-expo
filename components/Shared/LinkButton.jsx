fc171b293d94c9eb4557d08665e98af87356406a
sakib-repliq <sakibul.islam@repliq.dev>
2023-09-15 12:10:26 +0600

    Created link button component

--- components/Shared/LinkButton.jsx
+++ components/Shared/LinkButton.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "expo-router";
+import { Text } from "react-native";
 
-const LinkButton = () => {
-  return <div></div>;
+const labelVariants = {
+  default: "text-grey-500",
+  teal: "text-primary font-semibold",
+};
+
+const LinkButton = ({
+  buttonLabel,
+  variant = "default",
+  href,
+  extraClassName,
+}) => {
+  return (
+    <Link href={href} asChild>
+      <Text className={`${labelVariants[variant]} ${extraClassName}`}>
+        {buttonLabel}
+      </Text>
+    </Link>
+  );
 };
 
 export default LinkButton;
